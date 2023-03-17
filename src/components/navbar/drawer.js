66a3fc4eed68130dc7e4a6a7142de43045d1fd77
AMAR <amar@AMARs-MacBook-Pro.local>
2023-03-18 04:00:20 +0530

    changes-resume

--- src/components/navbar/drawer.js
+++ src/components/navbar/drawer.js
@@ -5,30 +5,59 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import { Link } from "react-scroll";
+import { ListItemIcon, Typography } from "@mui/material";
+import DownloadIcon from "@mui/icons-material/Download";
+import resume from "../../assets/pdf/resume.pdf";
 
 const NavMenuList = ({ toggleDrawer, navMenuItem }) => {
   return (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer} onKeyDown={toggleDrawer}>
       <List>
         <nav id="nav-menu">
-          {navMenuItem.map((item) => (
-            <Link
-              activeClass="active"
-              smooth
-              spy
-              to={item.id}
-              className={`nav-link ${item.id}`}
-              key={item.id}
-              onClick={toggleDrawer}
-              onKeyDown={toggleDrawer}
-            >
-              <ListItem key={item.id} disablePadding>
-                <ListItemButton>
-                  <ListItemText primary={item.title} />
-                </ListItemButton>
-              </ListItem>
-            </Link>
-          ))}
+          {navMenuItem.map((item) =>
+            item?.id === "resume" ? (
+              <a
+                href={resume}
+                download="Amarjeet-Kumar-Resume"
+                target="blank"
+                className={`nav-link ${item.id}`}
+                key={item.id}
+              >
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText
+                      disableTypography
+                      primary={
+                        <Typography variant="inherit" color="primary">
+                          {item.title}
+                        </Typography>
+                      }
+                    />
+                    <ListItemIcon>
+                      <DownloadIcon color="primary" />
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+              </a>
+            ) : (
+              <Link
+                activeClass="active"
+                smooth
+                spy
+                to={item.id}
+                className={`nav-link ${item.id}`}
+                key={item.id}
+                onClick={toggleDrawer}
+                onKeyDown={toggleDrawer}
+              >
+                <ListItem key={item.id} disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary={item.title} />
+                  </ListItemButton>
+                </ListItem>
+              </Link>
+            )
+          )}
         </nav>
       </List>
     </Box>
