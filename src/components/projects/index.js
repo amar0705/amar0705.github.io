449d4c926dd8475374d49c44725e82c4897e574c
AMAR <amar@AMARs-MacBook-Pro.local>
2023-08-03 23:07:16 +0530

    project modification

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -11,46 +11,18 @@ import node from "../../assets/skills/nodejs-logo-1.png";
 import express from "../../assets/skills/ExpressJS.png";
 import mongo from "../../assets/skills/mongo.png";
 import optimumFitnessImg from "../../assets/projects/optimum-fitness.jpg";
-import meatMeOnceImg from "../../assets/projects/meat-me-once.jpeg";
+import salonlexImg from "../../assets/projects/salonlex.png";
 
 const Projects = () => {
   const projectsList = [
     {
-      id: "meat-me-once",
-      title: "Meat Me Once",
+      id: "salonlex",
+      title: "Salonlex",
       description:
-        "Meat delivery Licious clone 'Meat Me Once'. A licious clone having features of adding the products to cart and UI/UX clone according to licious.",
-      image: meatMeOnceImg,
-      github: "https://github.com/amar0705/spurious-picture-648",
-      live: "https://flourishing-puppy-c8ecbd.netlify.app/",
-      techStack: [
-        { name: "HTML5", logo: html },
-        { name: "CSS3", logo: css },
-        { name: "JavaScript", logo: javascript },
-      ],
-    },
-    {
-      id: "optimum-fitness",
-      title: "Optimum Fitness",
-      description:
-        "Eat smater, Live better. A website where you can track your diet, exercise,and health data. Have a alot of features to check on your used calories and calculate bmi etc.",
-      image: fitoryxImg,
-      github: "https://github.com/Sayeda-5/Sayeda-5-envious-society-6497-",
-      live: "https://singular-babka-0ed0b3.netlify.app/",
-      techStack: [
-        { name: "HTML5", logo: html },
-        { name: "CSS3", logo: css },
-        { name: "JavaScript", logo: javascript },
-      ],
-    },
-    {
-      id: "better-buys",
-      title: "Better Buys",
-      description:
-        "An e-commerce website project for buying the product, along with the proper support of backend API's and storing the data in Mongo Atlas.",
-      image: betterBuysImg,
-      github: "https://github.com/amar0705/tangible-robin-3650",
-      live: "https://quiet-phoenix-56b570.netlify.app/",
+        "A hair salon website that enables customers to book appointments online. The platform showcases available time slots, stylists, allowing for a seamless user experience.",
+      image: salonlexImg,
+      github: "https://github.com/amar0705/nice-flesh-360",
+      live: "https://salonlex.netlify.app/",
       techStack: [
         { name: "HTML5", logo: html },
         { name: "CSS3", logo: css },
@@ -75,6 +47,37 @@ const Projects = () => {
         { name: "Node JS", logo: node },
         { name: "Express", logo: express },
         { name: "MonogoDB", logo: mongo },
+      ],
+    },
+    {
+      id: "better-buys",
+      title: "Better Buys",
+      description:
+        "An e-commerce website project for buying the product, along with the proper support of backend API's and storing the data in Mongo Atlas.",
+      image: betterBuysImg,
+      github: "https://github.com/amar0705/tangible-robin-3650",
+      live: "https://quiet-phoenix-56b570.netlify.app/",
+      techStack: [
+        { name: "HTML5", logo: html },
+        { name: "CSS3", logo: css },
+        { name: "JavaScript", logo: javascript },
+        { name: "Node JS", logo: node },
+        { name: "Express", logo: express },
+        { name: "MonogoDB", logo: mongo },
+      ],
+    },
+    {
+      id: "optimum-fitness",
+      title: "Optimum Fitness",
+      description:
+        "Eat smater, Live better. A website where you can track your diet, exercise,and health data. Have a alot of features to check on your used calories and calculate bmi etc.",
+      image: fitoryxImg,
+      github: "https://github.com/Sayeda-5/Sayeda-5-envious-society-6497-",
+      live: "https://singular-babka-0ed0b3.netlify.app/",
+      techStack: [
+        { name: "HTML5", logo: html },
+        { name: "CSS3", logo: css },
+        { name: "JavaScript", logo: javascript },
       ],
     },
   ];
